34deeb8c819f22f253da6c82cd3151f3719af680
Arkadiusz Żmudzin <arek@moodup.team>
2023-11-18 18:59:02 +0100

    add neogit plugin

--- init.lua
+++ init.lua
@@ -238,6 +238,17 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",       -- required
+      "nvim-telescope/telescope.nvim", -- optional
+      "sindrets/diffview.nvim",      -- optional
+      "ibhagwan/fzf-lua",            -- optional
+    },
+    config = true
+  }
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
